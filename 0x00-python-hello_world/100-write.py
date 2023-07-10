3dbff56c91530cd51dd2baa04f9a1a0f06916647
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-10 20:53:36 +0400

    100-write.py - 01

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def a_RT():
     _string = "and that piece of art is useful - Dora Korpar, 2015-10-19"
     sys.stderr.write(_string + "\n")
     sys.exit(1)
+
 
 a_RT()
