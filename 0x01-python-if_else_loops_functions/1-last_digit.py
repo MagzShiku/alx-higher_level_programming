48ad0185f9e94a1e7e9a3aca25665b75bc67fbb5
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-11 12:33:20 +0400

    1-last_digit.py - 09

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,5 @@ if suffix == 0:
 elif suffix > 5 and suffix % 10 != 0:
     print(f"Last digit of {number:d} is {suffix:d} and is greater than 5")
 else:
-    print(f"Last digit of {number:d} is {suffix:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {suffix:d} and\
+is less than 6 and not 0")
