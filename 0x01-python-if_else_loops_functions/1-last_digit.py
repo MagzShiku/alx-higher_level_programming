1df5866dce51619251774c776a1fa4f0aedfb17a
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-11 10:54:55 +0400

    1-last_digit.py - 01

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 suffix = abs(number) % 10
+if number < 0:
+    suffix = -suffix
+    print(f"Last digit of {number:d} is {suffix:d} and is ", end="")
 if suffix > 5:
-    suffix = suffix * -1
-    print(f"Last digit of {number:d} is {suffix:d} and is greater than 5")
+    print("greater than 5")
 elif suffix == 0:
-    print(f"Last digit of {number:d} is {suffix:d} and is 0")
-elif suffix < 0 and not 0:
-    print(f"Last digit of {number:d} is {suffix:d} and is less than 6 and not 0")
+    print("0")
+else:
+    print("less than 6 and not 0")
