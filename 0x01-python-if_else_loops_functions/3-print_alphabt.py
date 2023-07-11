4a172c2afcf8a9ab8e1e67268fa5221e3f4aea79
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-11 13:06:42 +0400

    3-print_alphabt.py - 10

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 113):
-    print(f"{chr(i)}", end='')
-
-for i in range(114, 123):
-    print(f"{chr(i)}", end='')
-
-print()
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print("{:c}".format(i), end='')
