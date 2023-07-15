66062f781b128ebb363bff4f47e5c0abbaf013c9
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-15 22:33:33 +0400

    6-print_matrix_integer.py - 01 I am getting this

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for  the_row in matrix:
+    for the_row in matrix:
         for row_val in the_row:
             print(f"{row_val:d}", end="")
             if row_val != the_row[- 1]:
