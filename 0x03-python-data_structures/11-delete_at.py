41a458925d320f6314af8ff1caa94407d20c9c62
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-15 23:50:53 +0400

    11-delete_at.py - 09 I am getting this

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,4 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
     else:
-        return my_list.pop(idx)
+        return [my_list[i] for i in range(len(my_list)) if i != idx]
