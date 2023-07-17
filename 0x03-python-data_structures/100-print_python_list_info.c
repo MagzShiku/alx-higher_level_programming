70ee7b00b5a09cfcf6f0a84854cb2ffbd713aac0
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-17 19:03:38 +0400

    Strange

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,19 +5,19 @@ void print_python_list_info(PyObject *p)
 	int a;
 	int i;
 	int _mem;
-	PyObject *_obj;
+	PyObject *_object;
 
 	a = PyList_Size(p);
-	printf("[*] Size of the Python List = %d\n", a);
-
 	_mem = ((PylistObject *) p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", a);
 	printf("[*] Allocated = %d\n", _mem);
 
 	i = 0;
 	while (i < a)
 	{
 		_obj = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_Type(item)->tp_name):
+		printf("Element %ld: %s\n", i, Py_Type(item)->tp_name):
 		i++;
 	}
 }
