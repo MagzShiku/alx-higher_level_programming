7fcafa1c8770afad3f833cd35c32f46f407175a3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-18 13:44:36 +0300

    Function for MAgic - 02

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
 def magic_calculation(a, b):
     if a < b:
-        n = add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            n = add(n, i)
-        return n
+            c = add(c, i)
+        return c
     else:
         return sub(a, b)
