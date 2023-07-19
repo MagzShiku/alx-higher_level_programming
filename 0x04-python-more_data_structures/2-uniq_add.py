bde229c667d2384f29c4be06de98d6ab8a9060bc
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-19 13:54:43 +0300

    sum : unique integers in list - 00

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    ints_in_list = set()
+    idx = 0
+    while idx  < len(my_list):
+        if my_list[idx] not in ints_in_list:
+            ints_in_list.add(my_list[idx])
+        idx += 1
+    return sum(ints_in_list)
