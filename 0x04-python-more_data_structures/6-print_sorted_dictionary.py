93991ff825221ad24b4137b4c5547812c7a8b664
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-19 14:41:09 +0300

    ordered keys in dictionary, using .keys - 0

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    new_dict = sorted(a_dictionary.keys())
+    for n in new_dict:
+
+        idx = a_dictionary[n]
+        print("{}: {}".format(n, idx))
+        #print out the value of keys, orderd
