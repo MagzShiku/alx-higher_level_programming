0647dfff4ae2409bc1e74c36cec0e82b586f86b3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-19 16:00:45 +0300

    computes the weigyted averavge of a & b - 01

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,5 +3,6 @@ def weight_average(my_list=[]):
     if not my_list:
         return (0)
 
-    res = [a * b for a, b in my_list] # a & b represent score and weight
+    res = [a * b for a, b in my_list]
+    # a & b represent score and weight
     return (sum(res) / sum(b for a, b in my_list))
