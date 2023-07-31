80e2cdf9b750900fcc26c5599216190c316555ce
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-07-31 16:14:32 +0300

    try 09

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,12 +3,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    if not isinstance(value, int):
-        sys.stderr.write("Exception: input value must be an integer\n")
-        return False
     try:
         sys.stdout.write("{:d}\n".format(value))
-        return True
-    except ValueError as ve:
-        sys.stderr.write("Exception: {}\n".format(ve))
+    except (ValueError, TypeError) as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
+    return True
