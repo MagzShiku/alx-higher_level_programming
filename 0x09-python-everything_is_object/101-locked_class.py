9794dc022591440193497894028a844263ace9b4
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-08 18:10:44 +0300

    Advanced tasks - part 1 - 04

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -17,16 +17,3 @@ class LockedClass:
        can be defined in a class to specify a fixed set
        of attributes for instances of that class
     """
-    def __init__(self):
-        pass
-
-    def __setattr__(self, attri_bute, value):
-        if attri_bute == "first_name":
-            super().__setattr__(attri_bute, value)
-            """
-                super(),  built in function that returns a temporary
-                object of the superclass. It allows you to call methods
-            """
-        else:
-            raise AttributeError("'LockedClass' object has no \
-                    attribute '{}'".format(attri_bute))
