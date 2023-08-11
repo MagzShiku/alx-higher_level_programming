baf321cc4bfe905b323403093b7bbdab7c5b5657
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-11 08:35:16 +0300

    Test 4 - 07

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,11 @@
 
 
 def text_indentation(text):
-    """we define the function here, then the error message"""
+    """
+        we define the function here, then the error message
+        Write a function that prints a text with 2 new lines
+        after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
