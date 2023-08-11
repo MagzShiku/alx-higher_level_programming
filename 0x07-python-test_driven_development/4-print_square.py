eed0f304866104fbe0228a96d34f435f108d2fe3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-11 08:04:44 +0300

    Test 3 - 02

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,11 +4,12 @@
     Write a function that prints a square with the character #
     Prototype: def print_square(size):
     size is the size length of the square
-    size must be an integer, otherwise raise a TypeError 
+    size must be an integer, otherwise raise a TypeError
     exception with the message size must be an integer
     if size is less than 0, raise a ValueError exception
     with the message size must be >= 0
 """
+
 
 def print_square(size):
     if not isinstance(size, int):
