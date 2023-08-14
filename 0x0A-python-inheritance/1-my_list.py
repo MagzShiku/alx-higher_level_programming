8d8c5ebe0a34d1963c33dbaaaa1cb6944fa4cf36
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-14 13:39:22 +0300

    first add - 03

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-    a class MyList that inherits from list
+    a class MyList that inherits list
     Public instance method: def print_sorted(self):
     that prints the list, but sorted (ascending sort)
     You can assume that all the elements of the list will be of type int
-    no importing modules
 """
 
 
 class MyList(list):
-    """inherit from list class and add to new method print"""
+    """inherit list class and add to new method print"""
     def print_sorted(self):
         """print the sorted list in ascending order"""
         print(sorted(self))
