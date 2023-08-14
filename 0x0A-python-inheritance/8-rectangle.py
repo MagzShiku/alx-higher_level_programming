27034e621f7995fb93c3947ced3b30ea7e232758
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-14 15:13:08 +0300

    first add - 10

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    Write a class Rectangle that inherits from BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """define the class"""
+    def __init__(self, width, height):
+        """initialize the pobjec with width and height"""
+        self.__width = self.integer_validator('width', width)
+        self.__height = self.integer_validator('height', height)
+
+    def __str__(self):
+        """return string reping the rectangle"""
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
