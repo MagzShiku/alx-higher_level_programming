cbc0b97dbf7beeec4d45b729626dec834ee906d3
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-14 14:43:06 +0300

    first add - 09

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """
+        Write a class BaseGeometry (based on 5-base_geometry.py).
+    """
+    def area(self):
+        raise Exception('area() is not implemented')
