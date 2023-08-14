ee317295d00b5b649933aece48510105e80e1930
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-14 14:30:29 +0300

    first add - 07

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,5 +13,6 @@ def inherits_from(obj, a_class):
         the specified class ; otherwise False.
     """
 
-    return isinstance(obj, type) and issubclass(obj,
-            a_class) and type(obj) != a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
