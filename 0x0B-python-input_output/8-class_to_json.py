48fdc73fa2259762869d7624f462cd54fc0e0484
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-15 17:19:09 +0300

    we are here to input/output files - 09

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,13 +10,18 @@
 def class_to_json(obj):
     """returns dictionary with what I have named  above"""
     if not isinstance(obj, object):
+        # obj is the instance of a class to be converted into JSON
         return obj
 
     json_obj = {}
-
+    # initialized to store the converted data
+    
+    # iterate through the key_value pairs of the obj to get the data types
+    # if it has the data structure, it is added to json_obj directory
     for key_pair, value_pair in obj.__dict__.items():
         if isinstance(value_pair, (list, dict, str, int, bool)):
             json_obj[key_pair] = value_pair
         elif isinstance(value_pair, object):
             json_obj[kay_pair] = class_to_json(value_pair)
     return json_obj
+    # returns the variable json_obj which will contain converted data
