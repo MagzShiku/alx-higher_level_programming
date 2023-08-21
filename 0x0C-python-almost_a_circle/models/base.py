41e445c8614c4bd393e9854c27dc6b66ef32b787
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-21 10:45:46 +0300

    creating the base case - 00

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+"""
+this base class manages the id attribute in my future classes
+and to avoind duplicating the same code and some bugs by extension
+"""
+
+
+class Base:
+    __nb_objects = 0
+    """
+    this is a provateclass attribute method
+    this keeps track of the number of objectc created from this class
+    """
+    def __init__(self, id=None):
+        """we initialize method with init, and set id to None as default"""
+        if id is not None:
+            """
+            assigns the value of id to a public instance attribute self.id
+            otherwise, it increments it, iterating through the values
+            """
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            seld.id = Base.__nb_objects
