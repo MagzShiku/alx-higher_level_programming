dcd6da90c3a95db7cea66a4afab03297a90335d4
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-08-25 16:35:17 +0300

    rec 2 - 07

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,12 @@ class Base:
             content = [obj.to_dictionary() for obj in list_objs]
         with open(my_file, "w") as fl:
             fl.write(cls.to_json_string(content))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
