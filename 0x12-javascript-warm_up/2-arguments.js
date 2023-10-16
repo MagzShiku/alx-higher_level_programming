35d2b3e789357d96a50545dfee31a2295e87d274
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-10-16 11:44:25 +0300

    Task 02 : REV - 00

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that prints a message depending of the number of arguments passed
+
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
