3241d2e5e641dbdadf6b73d72e784db054b8089b
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-10-17 13:15:28 +0300

    Task 10: Rev 00

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 //  converts a number from base 10 to another base passed as argument
 
-
 exports.converter = function (base) {
   return function (number) {
     return number.toString(base);
