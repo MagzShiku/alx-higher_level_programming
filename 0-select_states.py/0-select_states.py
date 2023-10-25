f1c18730faa372af67c529771d3625647612c04c
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-10-25 13:17:22 +0000

    somethig is wrong

---
+++ 0-select_states.py/0-select_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+connects to a MySQL database and lists all states from the hbtn_0e_0_usa table
+"""
+
+import MySQLdb
+import sys
+
+# collect arguments from cmd line
+mysql_user = sys.argv[1]
+mysql_password = sys.argv[2]
+database_name = sys.argv[3]
+
+# connect to server
+try:
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name
+    )
+    cursor = db.cursor()
+
+    # exec querry of mysql to fetch states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # show what you have fetched
+    fetched = command.fetchall()
+    for line in fetched:
+        print(line)
+
+    # close connection between cursor and data base
+    cursor.close()
+    bd.close()
+
+except MySQLdb.Error as e:
+    print("Error connecting to MySQL database:", str(e))
