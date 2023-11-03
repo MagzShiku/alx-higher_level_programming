d81fcf7517c9ac66e790c2b2b81ee4a44995f883
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-03 04:29:08 -0700

    Task 100 - 01

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     commits = response.json()
 
     for commit in commits[:10]:
-        vvv = commit.get("vvv")
+        sha = commit.get("sha")
         author_name = commit.get("commit").get("author").get("name")
-        print(f"{vvv}: {author_name}")
+        print(f"{sha}: {author_name}")
