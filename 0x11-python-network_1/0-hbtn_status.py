13e74e06e3e02d0470590ccd10ed5c5f108f0114
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-03 03:22:25 -0700

    Task0 - 00

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    """
+    define the url
+    """
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
