0724556d344dae3776aa59707af3562122a3f798
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-05 03:34:20 -0800

    Task 1 -02

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the bod
-curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
+# takes in a URL, sends a GET request to the URL, and display body
+curl -s $1 | awk '/HTTP\/1.1 200 OK/ { flag=1; next } flag { print }'
