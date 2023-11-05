96238fd3ed43e65965930c5750ea842efb615c78
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-05 03:23:53 -0800

    Task 1 -01

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#  takes in a URL, sends a GET request to the URL, and displays the bod
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" == "200" ] && curl -s "$i"
-
+# takes in a URL, sends a GET request to the URL, and displays the bod
+curl -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
