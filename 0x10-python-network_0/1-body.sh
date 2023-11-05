9f5dd4f8af786a2ec6eb6e40bcd960dbc0974022
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-05 03:36:43 -0800

    Task 1 -02

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and display body
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
+curl -s -o /dev/null -w "%{http_code}" $1 | xargs -I % sh -c '[ "%" == "200" ] && curl -s $1'
