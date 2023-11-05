e458bc7312dc0db5febe596581d7b70888792da6
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-11-05 03:38:29 -0800

    T3sk 1 -02

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and display body
-curl -s -o /dev/null -w "%{http_code}" $1 | xargs -I % sh -c '[ "%" == "200" ] && curl -s $1'
+[ $(curl -s -o /dev/null -w "%{http_code}" $1) == '200' ] && curl -s $1
