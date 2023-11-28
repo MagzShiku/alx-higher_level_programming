05a2d2747174b9d24d9d47b71e239f1cfe3e117d
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 09:49:42 +0000

    trial -  to get url ststus code_rev_01

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,10 @@
 const request = require('request');
 
 // use the comd line args to get url request
-request.get(process.argv[2])
-	.on('response', function (response) {
-		// print the status code on console
-		console.log('code:, ${statusCode)}');
-	});
+request.get(process.argv[2], function (error, response) {
+	// check error
+	if (error) {
+		console.error(error);
+	}
+	console.log('code:', response && response.statusCode);
+});
