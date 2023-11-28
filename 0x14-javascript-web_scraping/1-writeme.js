86812d5f3ef1a33c324f10246a4557f9b0bf6b84
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 08:59:03 +0000

    writes a string to a file - rev_00

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// Import the fs module, which helps interract with file with things
+// such as write, read etc
+const fs = require('fs');
+
+// retrieve the file path and data from the cmd line args
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// write the content of the file
+// the write function will have 4 args: path, content, encoding, callback function
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+	//check for error
+	if (err) {
+		console.error(err);
+		return;
+	}
+	//else {
+	//	console.log('File has been written successfully!');
+	//}
+
+});
