c7492de378ac2358dbbe386146d057bc511fd72c
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-11-28 09:45:30 +0000

    trial -  to get url ststus code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,19 +3,9 @@
 // Import the request node for sending HTTP/HTTPS requests
 const request = require('request');
 
-// Retrieve url to request fom CMD line args
-const url = process.argv[2];
-
-// send a GET request
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  // retrieve status code from response object
-  const statusCode = response.statusCode;
-
-  // print the status to the consile
-  console.log('code:', statusCode);
-});
+// use the comd line args to get url request
+request.get(process.argv[2])
+	.on('response', function (response) {
+		// print the status code on console
+		console.log('code:, ${statusCode)}');
+	});
